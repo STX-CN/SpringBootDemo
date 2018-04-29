2ed85db37ce0f5b3896838af400aed763915b97a
zhujm <zhujm@smartcloud-china.com>
2018-04-29 11:58:42 +0800

    给整个类增加一个访问路径，然后其他分别设置对应的访问方法路径

--- src/main/java/com/example/demo/BaseController.java
+++ src/main/java/com/example/demo/BaseController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/demo")
 public class BaseController {
 	
 	@Autowired
