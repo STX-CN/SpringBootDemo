948be84e80a8d0e5efddae305bf72d1bc2eacb91
zhujm <zhujm@smartcloud-china.com>
2018-04-29 12:15:12 +0800

    @RequestParam

--- src/main/java/com/example/demo/BaseController.java
+++ src/main/java/com/example/demo/BaseController.java
@@ -1,20 +1,17 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/demo")
 public class BaseController {
 	
-	@Autowired
-	private PeopleProperties peopleProp;
-	
-	@RequestMapping(value= "/hello/{id}", method = RequestMethod.GET)
-	public String say(@PathVariable("id")int id)
+	@RequestMapping(value= "/hello", method = RequestMethod.GET)
+	public @ResponseBody String say(@RequestParam("id")int id)
 	{
 		return "id:" + id;
 	}
