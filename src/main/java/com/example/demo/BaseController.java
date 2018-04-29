934a8fc811ca153bff97782c99d524c11e9d07cd
zhujm <zhujm@smartcloud-china.com>
2018-04-29 11:09:23 +0800

    1. 将application.properties替换为application.yml
    2  配置文件注入变量

--- src/main/java/com/example/demo/BaseController.java
+++ src/main/java/com/example/demo/BaseController.java
@@ -1,16 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
-
+	
+	@Value("${age}")
+	private int age;
+	
+	@Value("${nation}")
+	private String nation;
+	
+	@Value("${content}")
+	private String content;
+	
 	@RequestMapping(value="/hello", method = RequestMethod.GET)
 	public String say()
 	{
-		return "hello spring boot...";
+		return content;
 	}
+	
+	
 	
 }
