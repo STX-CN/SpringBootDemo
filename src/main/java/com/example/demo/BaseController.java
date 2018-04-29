0bee87f78adb51e5ac09683693906a5d1c52e425
zhujm <zhujm@smartcloud-china.com>
2018-04-29 11:56:45 +0800

    @RequestMapping支持多个访问集合

--- src/main/java/com/example/demo/BaseController.java
+++ src/main/java/com/example/demo/BaseController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,7 @@ public class BaseController {
 	@Autowired
 	private PeopleProperties peopleProp;
 	
-	@RequestMapping(value="/hello", method = RequestMethod.GET)
+	@RequestMapping(value= {"/hello", "hi"}, method = RequestMethod.GET)
 	public String say()
 	{
 		return peopleProp.getNation() + peopleProp.getAge();
